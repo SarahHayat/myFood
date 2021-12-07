5f3378096b4655d005892042998e0de8e5ece306
Resxt <55228336+Resxt@users.noreply.github.com>
2021-12-07 15:59:48 +0100

    Meal detail page prototype
    MealPageComponent function

---
+++ api/utils.js
@@ -0,0 +1,29 @@
+function apiStringsToArray(data, key, startIndex, endIndex) {
+    let elements = [];
+
+    for (let i = startIndex; i <= endIndex ; i++) {
+        let element = data[key + `${i}`];
+
+        if (element !== "" && element !== null && element !== undefined)
+        {
+            elements.push(element);
+        }
+    }
+
+    return elements
+}
+
+export function parseMealData(data) {
+    return {
+        id: data.idMeal,
+        name: data.strMeal,
+        category: data.strCategory,
+        origin: data.strArea,
+        instructions: data.strInstructions,
+        imageUrl: data.strMealThumb,
+        tags: data.strTags.split(','),
+        youtubeUrl: data.strYoutube,
+        ingredients: apiStringsToArray(data, 'strIngredient', 1, 20),
+        measures: apiStringsToArray(data, 'strMeasure', 1, 20),
+    }
+}
