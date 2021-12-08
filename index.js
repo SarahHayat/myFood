fd44fca398e318e4377bcc26ade6ddc33b8e148e
Lucas <lucas@ipedis.com>
2021-12-08 10:15:17 +0100

    Feature list recettes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import listRecettes from './android/app/src/components/listRecettes';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => listRecettes);
